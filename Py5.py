5ff9e01ac5b1314120009a0bb55a016d7637245a
Sergei Ustenko <Ustenko.sv@gmail.com>
2022-04-25 16:43:28 +0300

    dsfsdfd

--- Py5.py
+++ Py5.py
@@ -4,3 +4,9 @@ scary_func = lambda num: 'divided by 2 and 3' \
     else 'divided by 3' if num % 3 == 0 \
     else 'not divided by 2 nor 3'
 print(scary_func(5679))
+
+full_func = lambda *args, **kwargs: (args, kwargs)
+ 
+print(full_func(1,5,6,7,name='Ivan', age=25))
+# Будет напечатано:
+# ((1, 5, 6, 7), {'name': 'Ivan', 'age': 25})
