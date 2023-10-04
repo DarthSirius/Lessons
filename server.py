b799580ed10db6b17684663da4bf959af4ec9b1c
DarthSirius <Ustenko-S@gaz-is.ru>
2023-10-04 10:35:59 +0300

    DS_PROD

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+with open('model.pkl', 'rb') as pkl_file:
+    model = pickle.load(pkl_file)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = np.array(request.json)
+    features = features.reshape(1, 4) 
+    num = model.predict(features)
+
+    return jsonify({
+        'prediction': num[0]
+    })
+
+if __name__ == '__main__':
+    app.run('localhost', 5000)
