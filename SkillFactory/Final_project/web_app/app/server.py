7ddd47657765cc3eb523478ece414427e3ec0f0b
DarthSirius <Ustenko-S@gaz-is.ru>
2023-11-17 16:17:54 +0300

    Diplom

---
+++ SkillFactory/Final_project/web_app/app/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+with open('./models/model.pkl', 'rb') as pkl_file:
+    model = pickle.load(pkl_file)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = np.array(request.json)
+    features = features.reshape(1, 33) 
+    num = model.predict(features)
+
+    return jsonify({
+        'prediction': round(num[0])
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
